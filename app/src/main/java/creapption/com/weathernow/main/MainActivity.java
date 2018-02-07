1c9ebc7de18f8a4c613e6ae14c8be7b9176faba5
brayanl <godsarmybl@gmail.com>
2018-02-07 02:49:32 -0500

    Add libraries support and change ic_launcher

--- app/src/main/java/creapption/com/weathernow/main/MainActivity.java
+++ app/src/main/java/creapption/com/weathernow/main/MainActivity.java
@@ -1,7 +1,9 @@
-package creapption.com.weathernow;
+package creapption.com.weathernow.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import creapption.com.weathernow.R;
 
 public class MainActivity extends AppCompatActivity {
 
